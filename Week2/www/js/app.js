54d0ec0b90de85b64f3ba095c1ea1fa5fb230e75
pmulders1 <pmulders1@student.avans.nl>
2016-02-22 10:39:17 +0100

    Lekker man

--- Week2/www/js/app.js
+++ Week2/www/js/app.js
@@ -1,11 +1,24 @@
-var arr = [];
+/*
+
+Vragen aan stijn:
+Hoe kunnen we mooi het click event oplossen van de li?
+	Op deze manier krijg je honderden click events die op alle afgaan.
+
+*/
 
 $(function(){
-  $( ".box" ).bind( "taphold", tapholdHandler );
- 
-  function tapholdHandler( event ){
-  	arr.push(event.target.innerHTML);
-    window.location.href = "#settings";
-    console.log(arr);
-  }
+
+	var index = 0;
+	$( ".box" ).bind( "taphold", tapholdHandler );
+
+	function tapholdHandler( event ){
+		$('#toWatchList').append('<li class="liDing">' + event.target.innerHTML+ '<span class="spanRight" data-id="' + index + '" id="deleteLi">delete</span></li>').click(removeLi);
+		window.location.href = "#watchlist";
+		index += 1;
+	}
+
+	function removeLi( event ){
+		var id = $(event.target).data('id');
+		var elem = $("#toWatchList").find("[data-id='" + id + "']").parent().remove();
+	}
 });
